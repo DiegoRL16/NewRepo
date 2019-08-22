094e5949d33c0ca14e02418b4971d0490201ddc3
Diego Rosas <diego8242@gmail.com>
2019-08-22 10:10:00 -0500

    Se realizo el primer programa de consola haciendo uso de la estructura secuencial

---
+++ doNETDevelopment/ConsoleApp/EjemplosDeClase/SumaProductoNumeros.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.EjemplosDeClase
+{
+    /// <summary>
+    /// Realizar un programa para la carga de 2 numeros enteros por teclado
+    /// e imprimir la suma y el producto de estos.
+    /// </summary>
+    class SumaProductoNumeros
+    {
+        static void Main (string [] args)
+        {
+            //Declarar las variables 
+            int num1, num2; //-->   Guardar los datos introducidos por el teclado.
+            int suma, producto; //--> Guardar los resultados esperados.
+
+            //Asignar los valores a las variables
+            Console.Write("Ingrese primer valor: ");
+
+            //Recibe la informacion desde la consola y lo almacena
+            //Convertir lo obtenido al esperado.
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese segundo valor: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Realizar las operaciones correspondientes
+            suma = num1 + num2;
+            producto = num1 * num2;
+
+            //Mostrar los resultados 
+            Console.WriteLine("La suma de los 2 valores es: " + suma);
+            Console.WriteLine("El producto de los 2 valores es: " + producto);
+
+            //Detener la consola
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
