89819d49d17828579e5426ebcd7a7bdfa05973c2
Diego Rosas <diego8242@gmail.com>
2019-08-22 18:30:13 -0500

    Se realizo el programa de Estructura Condicional Simple

---
+++ doNETDevelopment/ConsoleApp/EjemplosDeClase/EstructuraCondicionalSimple.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.EjemplosDeClase
+{
+    /// <summary>
+    /// Programa para ingresar el sueldo de una persona, si supera los 3000 pesos
+    /// mostrar un mensaje en pantalla indicando que debe abonar impuestos.
+    /// </summary>
+    class EstructuraCondicionalSimple
+    {
+        static void Main (string[] args)
+        {
+            //Declarar las variables
+            float sueldo;
+
+            //Preguntar el sueldo de la persona
+            Console.Write("Ingrese el sueldo: ");
+            sueldo = float.Parse(Console.ReadLine());
+
+            //Hacer la condicional para validar el sueldo, por condicional
+            if (sueldo > 3000)
+            {
+                //(1) SALIDA: --> Verdadero
+                Console.WriteLine("Esta persona debe abonar impuestos");
+            }
+
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
