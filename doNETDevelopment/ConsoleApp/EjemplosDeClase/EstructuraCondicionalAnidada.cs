6f927f7f72467effced9bc4da3b7695a66c6209d
Diego Rosas <diego8242@gmail.com>
2019-08-23 17:42:26 -0500

    Se realizo el ejemplo de la Estructura Condicional Anidada

---
+++ doNETDevelopment/ConsoleApp/EjemplosDeClase/EstructuraCondicionalAnidada.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.EjemplosDeClase
+{
+    /// <summary>
+    /// Programa que pida por teclado 3 notas de un alumno, calcule el promedio e imprima
+    /// alguno de estos mensajes:
+    /// Si el promedio es >= 7 mostrar "Promocionado"
+    /// Si el promedio es >= 4 y menor a 7 mostrar "Regular"
+    /// Si el promedio es menor a 4 mostrar "Reprobado"
+    /// </summary>
+    class EstructuraCondicionalAnidada
+    {
+        static void Main (string[] args)
+        {
+            //Declarar las variables
+            int n1, n2, n3;
+            int prom;
+
+            //Asignar las variables
+            Console.Write("Ingrese primer Nota: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese segunda Nota: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese tercer Nota: ");
+            n3 = Convert.ToInt32(Console.ReadLine());
+
+            //Realizar las operaciones correspondientes
+            prom = (n1 + n2 + n3) / 3;
+
+            //Validacion para mostrar el mensaje correspondiente 
+            if (prom >= 7)
+            {
+                Console.WriteLine("Promocionado");
+            }
+            else
+            {
+                //Condicion Anidada
+                if (prom >= 4 && prom <7)
+                {
+                    Console.WriteLine("Regular");
+                }
+                else
+                {
+                    Console.WriteLine("Reprobado");
+                }
+            }
+
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
