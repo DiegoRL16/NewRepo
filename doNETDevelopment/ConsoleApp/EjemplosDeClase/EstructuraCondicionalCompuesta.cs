17041e2941734d76d434d4133acaafcfa0faf915
Diego Rosas <diego8242@gmail.com>
2019-08-23 12:14:53 -0500

    Se realizo un ejercicio por consola aplicando estructura condicional compuesta.

---
+++ doNETDevelopment/ConsoleApp/EjemplosDeClase/EstructuraCondicionalCompuesta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.EjemplosDeClase
+{
+    /// <summary>
+    /// Programa que solicite ingresar 2 numeros distintos y muestre por pantalla
+    /// el mayor de ellos.
+    /// </summary>
+    class EstructuraCondicionalCompuesta
+    {
+        static void Main (string[] args)
+        {
+            //Declarar variables
+            int num1, num2;
+
+            //Asignarles un valor
+            Console.Write("Ingrese primer valor: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese segundo valor: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Validar cual de los dos es mayor
+            if (num1 > num2)
+            {
+                //(1) SALIDA: Verdadero
+                Console.WriteLine("El numero mayor es: " + num1);
+            }
+            else
+            {
+                //(2) SALIDA: Falso
+                Console.WriteLine("El numero mayor es: " + num2);
+            }
+
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
--- doNETDevelopment/ConsoleApp/EjemplosDeClase/EstructuraCondicionalSimple.cs
+++ doNETDevelopment/ConsoleApp/EjemplosDeClase/EstructuraCondicionalSimple.cs
@@ -12,24 +12,24 @@ namespace ConsoleApp.EjemplosDeClase
     /// </summary>
     class EstructuraCondicionalSimple
     {
-        static void Main (string[] args)
-        {
-            //Declarar las variables
-            float sueldo;
+        //static void Main (string[] args)
+        //{
+        //    //Declarar las variables
+        //    float sueldo;
 
-            //Preguntar el sueldo de la persona
-            Console.Write("Ingrese el sueldo: ");
-            sueldo = float.Parse(Console.ReadLine());
+        //    //Preguntar el sueldo de la persona
+        //    Console.Write("Ingrese el sueldo: ");
+        //    sueldo = float.Parse(Console.ReadLine());
 
-            //Hacer la condicional para validar el sueldo, por condicional
-            if (sueldo > 3000)
-            {
-                //(1) SALIDA: --> Verdadero
-                Console.WriteLine("Esta persona debe abonar impuestos");
-            }
+        //    //Hacer la condicional para validar el sueldo, por condicional
+        //    if (sueldo > 3000)
+        //    {
+        //        //(1) SALIDA: --> Verdadero
+        //        Console.WriteLine("Esta persona debe abonar impuestos");
+        //    }
 
-            Console.WriteLine("Presione una tecla para salir");
-            Console.ReadKey();
-        }
+        //    Console.WriteLine("Presione una tecla para salir");
+        //    Console.ReadKey();
+        //}
     }
 }
