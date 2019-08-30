75bb40dd69e8a42a2c966a6ba87af20f64a1b74a
Diego Rosas <diego8242@gmail.com>
2019-08-30 17:41:35 -0500

    Termine ejemplo de Estructura de Opcion Multiple

---
+++ doNETDevelopment/ConsoleApp/EjemplosDeClase/EstructuraOpcionMultiple.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.EjemplosDeClase
+{
+    class EstructuraOpcionMultiple
+    {
+        /// <summary>
+        /// Programa que reciba un numero del 1 al 5 y devolver un string con el 
+        /// nombre del numero. Mostrar en pantalla
+        /// </summary>
+        /// <param name="args"></param>
+        //static void Main (string[] args)
+        //{
+        //    //Declarar variables
+        //    int num;
+
+        //    //Asignarle un valor a las variables
+        //    Console.Write("Ingrese un numero (1-5): ");
+        //    num = Convert.ToInt32(Console.ReadLine());
+
+        //    //Aplicar opcion multiple
+        //    switch (num)
+        //    {
+        //        //Casos con cada una de las opciones
+        //        case 1:
+        //            Console.WriteLine("Uno");
+        //            break; //Detener el proceso
+
+        //        case 2:
+        //            Console.WriteLine("Dos");
+        //            break;
+
+        //        case 3:
+        //            Console.WriteLine("Tres");
+        //            break;
+
+        //        case 4:
+        //            Console.WriteLine("Cuatro");
+        //            break;
+
+        //        case 5:
+        //            Console.WriteLine("Cinco");
+        //            break;
+
+        //        //En caso de que no seleccionen un numero 1-5 mostrar este mensaje
+        //        default:
+        //            Console.WriteLine("Favor de ingresar un numero entre 1-5");
+        //            break;
+        //    }
+
+        //    Console.WriteLine("Presione una tecla para salir");
+        //    Console.ReadKey();
+        //}
+    }
+}
