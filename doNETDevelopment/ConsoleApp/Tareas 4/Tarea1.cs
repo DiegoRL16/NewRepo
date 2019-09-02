135a05251696efb28d92e788d9f8004af6894a40
Diego Rosas <diego8242@gmail.com>
2019-09-02 13:24:30 -0500

    Se realizo la tarea 1 y 2 de Tareas 4 correctamente.

---
+++ doNETDevelopment/ConsoleApp/Tareas 4/Tarea1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Tareas_4
+{
+    /// <summary>
+    /// Programa que pida un numero del 1 al 7 y diga el dia de la semana correspondiente
+    /// </summary>
+    class Tarea1
+    {
+        //static void Main(string[] args)
+        //{
+        //    int num;
+
+        //    Console.Write("Introduzca un numero (1-7): ");
+        //    num = Convert.ToInt32(Console.ReadLine());
+
+        //    switch (num)
+        //    {
+        //        case 1:
+        //            Console.WriteLine("Lunes");
+        //            break;
+        //        case 2:
+        //            Console.WriteLine("Martes");
+        //            break;
+        //        case 3:
+        //            Console.WriteLine("Miercoles");
+        //            break;
+        //        case 4:
+        //            Console.WriteLine("Jueves");
+        //            break;
+        //        case 5:
+        //            Console.WriteLine("Viernes");
+        //            break;
+        //        case 6:
+        //            Console.WriteLine("Sábado");
+        //            break;
+        //        case 7:
+        //            Console.WriteLine("Domingo");
+        //            break;
+        //    }
+
+        //    Console.WriteLine("Presione una tecla para salir");
+        //    Console.ReadKey();
+        //}
+    }
+}
---
+++ doNETDevelopment/ConsoleApp/Tareas 4/Tarea2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Tareas_4
+{
+    /// <summary>
+    /// Programa que muestre un menu de opciones como el siguiente:
+    ///1 - Suma
+    ///2 - Resta
+    ///3 - Multiplicacion
+    ///4 - Division
+    ///Y haga la operacion correspondiente a la opcion elegida, tomando como datos de
+    ///entrada 2 numeros enteros.
+    /// </summary>
+    class Tarea2
+    {
+        //static void Main (string[] args)
+        //{
+        //    int p, num1, num2;
+        //    int sum, res, mul, div;
+
+        //    Console.Write("Ingrese el primer valor: ");
+        //    num1 = Convert.ToInt32(Console.ReadLine());
+
+        //    Console.Write("Ingrese el segundo valor: ");
+        //    num2 = Convert.ToInt32(Console.ReadLine());
+
+        //    Console.WriteLine("Este es el menu a elegir!");
+        //    Console.WriteLine("1. Suma");
+        //    Console.WriteLine("2. Resta");
+        //    Console.WriteLine("3. Multiplicacion");
+        //    Console.WriteLine("4. División");
+
+        //    Console.Write("Ingrese la opcion deseada: ");
+        //    p = Convert.ToInt32(Console.ReadLine());
+
+        //    sum = num1 + num2;
+        //    res = num1 - num2;
+        //    mul = num1 * num2;
+        //    div = num1 / num2;
+
+        //    switch (p)
+        //    {
+        //        case 1:
+        //            Console.WriteLine("El resultado de la suma de los dos numeros ingresados es: " + sum);
+        //            break;
+        //        case 2:
+        //            Console.WriteLine("El resultado de la resta de los dos numeros ingresados es: " + res);
+        //            break;
+        //        case 3:
+        //            Console.WriteLine("El resultado de la multiplicacion de los dos numeros ingresados es: " +  mul);
+        //            break;
+        //        case 4:
+        //            Console.WriteLine("El resultado de la division de los dos numeros ingresados es: " + div);
+        //            break;
+        //    }
+
+        //    Console.WriteLine("Presione una tecla para salir");
+        //    Console.ReadKey();
+        //}
+    }
+}
