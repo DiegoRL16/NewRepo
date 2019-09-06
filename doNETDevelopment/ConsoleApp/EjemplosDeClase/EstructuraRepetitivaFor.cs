c4988d8140ebe00559314d7ff95f1efa8cebc843
Diego Rosas <diego8242@gmail.com>
2019-09-06 17:54:08 -0500

    Se realizo el ejemplo de Estructura Repetitiva For

---
+++ doNETDevelopment/ConsoleApp/EjemplosDeClase/EstructuraRepetitivaFor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.EjemplosDeClase
+{
+    /// <summary>
+    /// Programa que permita la carga de 10 valores por teclado y nos muestre 
+    /// posteriormente la suma de los valores ingresados y sus promedios.
+    /// </summary>
+    class EstructuraRepetitivaFor
+    {
+        static void Main (string[] args)
+        {
+            //Declarar las variables
+            int suma, promedio, valor;
+
+            //Inicializar valores de las variables
+            suma = 0;
+            promedio = 0;
+
+            //Aplicar la estructura repetitiva For
+            //Inicializador --> donde va a empezar a contar
+            //Condicion --> cuando se va a detener
+            //Incremento --> de cuanto en cuanto va a aumentar (i++ --> i=i+1)
+            for (int i = 0; i < 10; i ++)
+            {
+                //Asignar valores a las variables
+                Console.Write("Ingrese un valor: ");
+                valor = Convert.ToInt32(Console.ReadLine());
+
+                //Hacer la operacion de suma
+                suma = suma + valor;
+            }
+
+            Console.WriteLine("La suma es: " + suma);
+            promedio = suma / 10;
+            Console.WriteLine("El promedio es: " + promedio);
+
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
