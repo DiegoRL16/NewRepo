3309962343a72089990bd808f97a8b586e3ba83a
Diego Rosas <diego8242@gmail.com>
2019-09-12 18:20:02 -0500

    Se realizo el ejemplo de la Estructura Repetitiva Do While

---
+++ doNETDevelopment/ConsoleApp/EjemplosDeClase/EstructuraRepetitivaDoWhile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.EjemplosDeClase
+{
+    /// <summary>
+    /// Programa que solicite la carga de un numero entre 0 y 999 y nos muestre un mensaje de cuantos digitos 
+    /// tiene el mismo. Finalizar el programa cuando se cargue un valor igual a cero (0).
+    /// </summary>
+    class EstructuraRepetitivaDoWhile
+    {
+        //static void Main (string[] args)
+        //{
+        //    //Declara las variables
+        //    int valor;
+
+        //    //Aplicamos la estructura do while
+        //    do
+        //    {
+        //        //Asignar valores a las variables
+        //        Console.Write("Ingrese un valor entre 0 y 999 (0 finaliza): ");
+        //        valor = Convert.ToInt32(Console.ReadLine());
+
+        //        if (valor >= 100)
+        //        {
+        //            Console.WriteLine("Tiene 3 digitos");
+        //        }
+        //        else
+        //        {
+        //            if (valor >= 10)
+        //            {
+        //                Console.WriteLine("Tiene 2 digitos");
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("Tiene 1 digito");
+        //            }
+        //        }
+        //    } while (valor != 0); //Condicion que haga que se repita
+
+        //    Console.WriteLine("Presione una tecla para salir");
+        //    Console.ReadKey();
+        //}
+    }
+}
