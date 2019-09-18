d99974a2b1f1ce6a45bb1b81fd5cfbcfb4462f6f
Diego Rosas <diego8242@gmail.com>
2019-09-18 12:57:52 -0500

    Se realizaron cambios en los nombres de las carpetas , se Realizo el Ejemplo de Do While y la carpeta de Tareas 7 ejercicio 1 y 2 hechos!

---
+++ doNETDevelopment/ConsoleApp/Tareas 7 - EstructuraRepetitivaDoWhile/Tarea1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Tareas_7___EstructuraRepetitivaDoWhile
+{
+    /// <summary>
+    /// Programa que acumule (sume) valores ingresados por el teclado hasta ingresar el 
+    /// 9999 (No sumar dicho valor, indica que ha finalizado la carga). Imprimir el valor
+    /// acumulado e informar si dicho valor es cero, mayor a cero o menor a cero.
+    /// </summary>
+    class Tarea1
+    {
+        //static void Main (string[] args)
+        //{
+        //    int suma, valor;
+        //    suma = 0;
+
+        //    do
+        //    {
+        //        Console.Write("Ingrese un valor: ");
+        //        valor = Convert.ToInt32(Console.ReadLine());
+
+        //        if (valor <= 9998)
+        //        {
+        //            suma = suma + valor;
+        //        }
+
+        //    } while (valor != 9999);
+
+        //    if (suma > 0)
+        //    {
+        //        Console.WriteLine("El valor acumulado es mayor a cero");
+        //    }
+        //    else
+        //    {
+        //        if (suma < 0)
+        //        {
+        //            Console.WriteLine("El valor acumulado es menor a cero");
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("El valor acumulado es igual a cero");
+        //        }
+        //    }
+
+        //    Console.WriteLine("La suma del valor acumulado es: " + suma);
+
+        //    Console.WriteLine("Presione una tecla para salir");
+        //    Console.ReadKey();
+        //}
+    }
+}
---
+++ doNETDevelopment/ConsoleApp/Tareas 7 - EstructuraRepetitivaDoWhile/Tarea2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Tareas_7___EstructuraRepetitivaDoWhile
+{
+    /// <summary>
+    /// Checar en bloc de Notas
+    /// </summary>
+    class Tarea2
+    {
+
+        // NDC = Numero de Cuenta
+        // SA = Saldo Actual
+        // SU = Suma
+        static void Main(string[] args)
+        {
+            int NDC, SA, SU;
+
+            SU = 0;
+
+            do
+            {
+                Console.Write("Ingrese el Numero de Cuenta: ");
+                NDC = Convert.ToInt32(Console.ReadLine());
+
+                if (NDC >= 0)
+                {
+                    Console.Write("Ingrese el Saldo: $");
+                    SA = Convert.ToInt32(Console.ReadLine());
+
+                    if (SA > 0)
+                    {
+                        Console.WriteLine("Saldo Acreedor");
+                        SU = SU + SA;
+                    }
+                    else
+                    {
+                        if (SA < 0)
+                        {
+                            Console.WriteLine("Saldo Deudor");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Saldo Nulo");
+                        }
+                    }
+                }
+
+            } while (NDC >= 0);
+
+            Console.WriteLine("Total de saldos Acreedores: $" + SU);
+
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
